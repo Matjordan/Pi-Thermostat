cd7116bce46693e1945c664570826b6b33dbc43a
matjordan <matjordan3@gmail.com>
2016-02-10 23:30:57 -0500

    db stuff pass 2

--- source/Controller.py
+++ source/Controller.py
@@ -1,10 +1,58 @@
 
 import mysql.connector
-from datetime import date, datetime, timedelta,time
+import RPi.GPIO as GPIO
+import time
+
+
+HEATER_PIN = 26
+FAN_PIN = 20
+COMP_PIN = 21
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(HEATER_PIN, GPIO.OUT)
+GPIO.setup(FAN_PIN, GPIO.OUT)
+GPIO.setup(COMP_PIN, GPIO.OUT)
 
 database= mysql.connector.connect(user='root', password='thermo',host='127.0.0.1',database='thermostat')
 
 cursor = database.cursor()
-today=date(2016,1,1)
-hour=datetime.today().hour
 
+room =0
+
+def run():
+
+    query = ("SELECT on_off,fan,heat,cool FROM status WHERE room = '{}' ".format(room))
+    cursor.execute(query)
+    result=cursor.fetchall()
+    database.commit()
+    on_off = result[0][0]
+    fan = result[0][1]
+    heat = result[0][2]
+    cool = result[0][3]
+    
+    if cool:
+        GPIO.output(COMP_PIN, True)
+    else:
+        GPIO.output(COMP_PIN, False)
+
+    if heat:
+        GPIO.output(HEATER_PIN, True)
+    else:
+        GPIO.output(HEATER_PIN, False)
+
+    if fan:
+        GPIO.output(FAN_PIN, True)
+    else:
+        GPIO.output(FAN_PIN, False)
+        
+
+if __name__ == '__main__':
+     try:
+         while True:
+             run()
+             time.sleep(1)
+     finally:
+        print('bye')
+        GPIO.cleanup()
+        cursor.close()
+        database.close()
--- source/GUI/Thermostat_GUI.py
+++ source/GUI/Thermostat_GUI.py
@@ -21,69 +21,20 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import read_local_air as local_air
+#import read_local_air as local_air
 from queue import Queue
-import RPi.GPIO as GPIO
-import threading
 import time
 import os
 import sys
-from PyQt5.QtCore import pyqtSlot,SIGNAL,SLOT,Qt
-from PyQt5.QtGui import *
-
-ON = 1
-OFF = 0
-
-OFF_MODE = 0
-COOL_MODE = 1
-HEAT_MODE = 2
-
-global desired_temp
-desired_temp = 80
-
-COLD_THRESH = -2.0
-COOL_THRESH = -1.0
-WARM_THRESH = 1.0
-HOT_THRESH = 2.0
-
-COLD = -2
-COOL = -1
-NORMAL = 0
-WARM = 1
-HOT = 2
-
-COMP_OFF_TIME_THRESH = 15 * 60 #15 mins
-FAN_ON_TIME_THRESH = 2 * 60 #2 mins
-
-global ac_state 
-ac_state = OFF
-global fan_state
-fan_state = OFF
-global comp_state
-comp_state = OFF
-global heat_state
-heat_state = OFF
-
-global HEATER_PIN
-HEATER_PIN = 26
-global FAN_PIN
-FAN_PIN = 20
-global COMP_PIN
-COMP_PIN = 21
+from PyQt4.QtCore import pyqtSlot,SIGNAL,SLOT,Qt
+from PyQt4.QtGui import *
 
 
-#global set_mode
-set_mode = OFF_MODE
-#global loc_temp
-loc_temp = 0
-#global loc_feel
-loc_feel = 0
-#global loc_hum
-loc_hum = 0
-#global loc_press
-loc_press = 0
 
-read_ready=threading.Event()
+
+
+
+
 
 # create our window
 app = QApplication(sys.argv)
@@ -166,183 +117,7 @@ sett = QLabel(w)
 sett.setAlignment(Qt.AlignCenter)
 sett.setFont(font)
 
-def cls():
-    os.system(['clear','cls'][os.name == 'nt'])
 
-
-def apparent_temp(t,RH):
-	Tf=t*1.8+32
-
-	app_temp = -42.379+2.04901523*(Tf)+10.14333127*(RH)-0.22475541*(Tf)*(RH)-6.83783*10**(-3)*(Tf**(2))-5.481717*10**(-2)*(RH**(2))+1.22874*10**(-3)*(Tf**(2))*(RH)+8.5282*10**(-4)*(Tf)*(RH**(2))-1.99*10**(-6)*(Tf**(2))*(RH**(2))
-
-	return app_temp
-
-
-
-class temp_control(threading.Thread):
-
-    def __init__(self,event):
-        threading.Thread.__init__(self)
-        self.daemon = True
-        self.event=event
-        
-        global ac_state
-        ac_state = OFF
-        global fan_state
-        fan_state = OFF
-        global comp_state
-        comp_state = OFF
-
-
-    def run(self):
-        global loc_temp
-        global loc_hum
-        global loc_feel
-        global loc_press
-        global desired_temp
-
-        comp_on_time = 0
-        comp_off_time = 0
-        fan_on_time = 0
-        fan_off_time = 0
-
-        global ac_state
-        global fan_state
-        global comp_state
-        global heat_state
-        
-        global FAN_PIN
-        global COMP_PIN
-        global HEATER_PIN
-
-        while True:
-            
-
-            local = local_air.read_local_air()
-            local_temp_c = local[0]
-#            local_temp_c += -2
-            local_temp_f = local_temp_c*1.8+32
-            local_hum = local[1]
-            local_press = local[2]
-            local_feel_f =apparent_temp(local[0],local[1])
-            local_temp = local_temp_f
-                        
-            set_temp_f=desired_temp
-            
-            loc_temp = local_temp_f
-            loc_hum = local_hum
-            loc_feel = local_feel_f
-            loc_press = local_press            
-            self.event.set()
-
-            error_temp = local_temp - set_temp_f
-
-            fuzz_temp = NORMAL
-            if error_temp < COOL_THRESH:
-                    fuzz_temp = COOL
-            if error_temp < COLD_THRESH:
-                    fuzz_temp = COLD
-            if error_temp > WARM_THRESH:
-                    fuzz_temp = WARM
-            if error_temp > HOT_THRESH:
-                    fuzz_temp = HOT
-                 
-            if fan_state is ON:
-                    fan_on_time += 1
-            if fan_state is OFF:
-                    fan_off_time += 1
-            if comp_state is ON:
-                    comp_on_time += 1
-            if comp_state is OFF:
-                    comp_off_time += 1
-
-            if fan_state is ON:
-                GPIO.output(FAN_PIN, True)
-            else:
-                GPIO.output(FAN_PIN, False)
-            if comp_state is ON:
-                GPIO.output(COMP_PIN, True)
-            else:
-                GPIO.output(COMP_PIN, False)
-
-            if set_mode is COOL_MODE:
-                w.setPalette(coolPal)
-            
-                if ac_state is ON:
-                        if comp_state is ON:
-                                if fuzz_temp is COLD:
-                                        comp_state = OFF
-                                        comp_off_time = 0
-                                if fuzz_temp is COOL:
-                                        comp_state = OFF
-                                        comp_off_time = 0
-                                        fan_state = OFF
-                                        fan_off_time = 0
-                                        ac_state = OFF
-                        if comp_state is OFF:
-                                if fan_on_time > FAN_ON_TIME_THRESH:
-                                        if local_temp + 0.5 < fan_start_temp:
-                                                fan_start_temp = local_temp
-                                                fan_on_time = 0
-                                                if fuzz_temp is COOL or fuzz_temp is COLD:
-                                                        fan_state = OFF
-                                                        fan_off_time = 0
-                                                        ac_state = OFF
-                                        else:
-                                                comp_state = ON
-                                                comp_on_time = 0
-                                if fuzz_temp is HOT:
-                                        comp_state = ON
-                                        comp_on_time = 0
-                                if fuzz_temp is COOL or fuzz_temp is COLD:
-                                        fan_state = OFF
-                                        fan_on_time = 0
-                else:
-                        if fuzz_temp is WARM:
-                                if comp_off_time < COMP_OFF_TIME_THRESH:
-                                        ac_state = ON
-                                        fan_state = ON
-                                        fan_start_temp = local_temp
-                                        fan_on_time = 0 
-                                else:
-                                        ac_state = ON
-                                        fan_state = ON
-                                        fan_on_time = 0
-                                        comp_state = ON
-                                        comp_on_time = 0
-                        if fuzz_temp is HOT:
-                                ac_state = ON
-                                fan_state = ON
-                                fan_on_time = 0
-                                comp_state = ON
-                                comp_on_time = 0
-            if set_mode is HEAT_MODE:
-                #heater
-                #print(set_mode)
-                w.setPalette(heatPal)
-            if (set_mode is not 2) and (set_mode is not COOL_MODE):
-                w.setPalette(offPal)
-                fan_state = OFF
-                comp_state = OFF
-                ac_state = OFF
-                heat_state = OFF
-                
-#            #print(ac_state)
-#            #print(fan_state,fan_on_time,fan_off_time)
-#            #print(comp_state,comp_on_time,comp_off_time)
-#            time.sleep(1)
-
-def prints():
-    while True:
- #       f=read_ready.wait()
- #       read_ready.clear()
-        #cls()
-#        global desired_temp_f
-        global loc_temp
-        global loc_feel
-        global loc_hum
-        global loc_press
-        global fan_state
 
         #print('Temp = {0:0.2f} F'.format(loc_temp))
         #print('Feels = {0:0.2f} F'.format(loc_feel))
@@ -369,25 +144,7 @@ def decrease_temp(self):
 
 
 
-GPIO.setmode(GPIO.BCM)
 
-
-GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(HEATER_PIN, GPIO.OUT)
-GPIO.setup(FAN_PIN, GPIO.OUT)
-GPIO.setup(COMP_PIN, GPIO.OUT)
-
-GPIO.add_event_detect(17, GPIO.FALLING, callback=increase_temp, bouncetime=300)
-GPIO.add_event_detect(22, GPIO.FALLING, callback=decrease_temp, bouncetime=300)
-
-thr=temp_control(read_ready)
-thr.setDaemon(True)
-thr.start()
-time.sleep(2)
-thr2=threading.Thread(target = prints)
-thr2.setDaemon(True)
-thr2.start()
 
 
  
