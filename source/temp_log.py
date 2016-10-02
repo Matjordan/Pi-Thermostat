25a8548684aa743c3160532c914d31d3a7eeefa5
Matjordan <matjordan3@gmail.com>
2016-10-01 23:55:14 -0400

    Update temp_log.py

--- source/temp_log.py
+++ source/temp_log.py
@@ -2,3 +2,29 @@
 
 file = open('~/thermo_log.csv',w)
 
+import mysql.connector
+import time
+database= mysql.connector.connect(user='root', password='thermo',host='127.0.0.1',database='thermostat')
+
+cursor = database.cursor()
+room = 0
+while True:
+        query = ("SELECT temp,on_off,heat,cool,humidity,set_temp FROM status WHERE room = '{}' ".format(room))
+        cursor.execute(query)
+        result=cursor.fetchall()
+        database.commit()
+
+        real_temp = result[0][0]
+        on_off = result[0][1]
+        heat = result[0][2]
+        cool = result[0][3]
+        humidity = result[0][4]
+        set_temp = result[0][5]
+        
+        file.write(set_temp)
+        file.write(',')
+        file.write(real_temp)
+        file.write(',')
+        file.write(humidity)
+        file.write(';')
+        time.sleep(10)
